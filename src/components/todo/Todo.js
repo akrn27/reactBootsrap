30f14005ae0fdb1666024c4b8885d9f8e1992113
Akrn27 <akurnia755@gmail.com>
2022-01-19 16:32:55 +0700

    send data to the parent components

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -3,17 +3,21 @@ import TodoList from '../todo-list/TodoList';
 import './Todo.css';
 
 const Todo = () => {
-    const Todos = [
+    const todos = [
         { id: 1, title: 'Eat'},
         { id: 2, title: 'Sleep'},
         { id: 3, title: 'Code'},
     ]
 
+    const eventCreateTodo = (todo) => {
+        todos.push(todo)
+    }
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoCreate />
-            <TodoList dataTodos={Todos}/>
+            <TodoCreate onCreateTodo={eventCreateTodo}/>
+            <TodoList dataTodos={todos}/>
         </div>
     )
 }
