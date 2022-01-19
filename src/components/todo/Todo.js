b7dcb01f6de1a0ea96966d6f208a36b8d9b02ca0
Akrn27 <akurnia755@gmail.com>
2022-01-19 19:02:09 +0700

    use state for dynamic data

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,23 +1,25 @@
+import { useState } from "react";
 import TodoCreate from '../todo-create/TodoCreate';
 import TodoList from '../todo-list/TodoList';
 import './Todo.css';
 
 const Todo = () => {
-    const todos = [
+    const [getTodos, setTodos] = useState([
         { id: 1, title: 'Eat'},
         { id: 2, title: 'Sleep'},
         { id: 3, title: 'Code'},
-    ]
+    ])
 
     const eventCreateTodo = (todo) => {
-        todos.push(todo)
+        setTodos(getTodos.concat(todo));
+        console.log(getTodos);
     }
 
     return (
         <div>
             <h3>Todo List</h3>
             <TodoCreate onCreateTodo={eventCreateTodo}/>
-            <TodoList dataTodos={todos}/>
+            <TodoList dataTodos={getTodos}/>
         </div>
     )
 }
