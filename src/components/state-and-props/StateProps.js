9ad173c32ca3ab0ee98a12cde7c569801d942e62
Akrn27 <akurnia755@gmail.com>
2022-01-21 20:08:56 +0700

    state

--- src/components/state-and-props/StateProps.js
+++ src/components/state-and-props/StateProps.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+//state props
 export default class StateProps extends Component {
     constructor(props){
         super(props);
         this.state = {
             
         }
-    }
+    }//
 
 
     render() {
