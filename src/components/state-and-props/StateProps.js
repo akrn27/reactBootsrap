12e608a92fa0a8cfa1a9281b087f75e1caf00225
Akrn27 <akurnia755@gmail.com>
2022-01-23 15:38:28 +0700

    create object

--- src/components/state-and-props/StateProps.js
+++ src/components/state-and-props/StateProps.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-//state props
+
+//state propsgit st
 export default class StateProps extends Component {
     constructor(props){
         super(props);
         this.state = {
-            
+            makanan: 'Bakso'
         }
-    }//
+    }
 
 
     render() {
