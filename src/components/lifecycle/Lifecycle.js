8254601cdf42a622a9890686312ae0b865925320
Akrn27 <akurnia755@gmail.com>
2022-01-24 19:43:56 +0700

    create Sublifecycle.js

--- src/components/lifecycle/Lifecycle.js
+++ src/components/lifecycle/Lifecycle.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
+import Sublifecycle from "./Sublifecycle";
 
 export default class Lifecycle extends Component {
   constructor(props) {
     super(props)
     this.state = {
-       
+       makanan: 'Bakso',
+       data: {},
+       tampilHalamanSub: false
     }
   }
-  
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+        this.setState({
+            data : json
+        })
+    })
+  }
+
+  ubahMakanan(value) {
+      this.setState({
+          makanan: value
+      })
+  }
 
   render() {
     return (
       <div>
-        <h2>Halo</h2>
+        <h2>{this.state.makanan}</h2>
+        <Sublifecycle />
       </div>
     );
   }
