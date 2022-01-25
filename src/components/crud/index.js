e8e5739ad750c898010bc768adfea557e7b9c8cf
Akrn27 <akurnia755@gmail.com>
2022-01-25 17:06:28 +0700

    add search button

--- src/components/crud/index.js
+++ src/components/crud/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { NavbarComponent } from './NavbarComponent';
 
 export default class index extends Component {
   render() {
     return (
         <div>
+            <NavbarComponent />
             <h2>Halo ini adwad</h2>
         </div>
     )
